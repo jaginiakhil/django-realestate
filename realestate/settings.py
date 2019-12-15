abc18983288b12ae4b51a898f360b4f9b5a773c8
jaginiakhil <38194970+jaginiakhil@users.noreply.github.com>
2019-12-14 22:33:15 -0700

    Update settings.py

--- realestate/settings.py
+++ realestate/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'realestate.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'realestatedb',
+        'NAME': 'btre_prod',
         'USER': 'dbadmin',
         'PASSWORD': '12345',
         'HOST': 'localhost'
